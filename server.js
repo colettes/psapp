8e18005a3d371f6e2ea278a91b8d511b682ce3da
Colette Smith <Colette@colettes-mbp.attlocal.net>
2016-04-13 17:54:58 -0500

    added a photo

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+var photo = '<img src="http://travelnoire.com/wp-content/uploads/2014/04/Lisbon-Portugal.jpg" alt="LISBOA" style="width:300px;">';
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(photo);
 });
 
 app.listen(3000, function () {
